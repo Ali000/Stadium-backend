4a359b5aec81a8aed33f4907f88d3024436a85c4
najeeb2442 <s.najib2442@gmail.com>
2024-02-29 11:45:46 +0300

    models team, ticket, user done

--- models/Team.js
+++ models/Team.js
@@ -1,2 +1,15 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const teamSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    sport: { type: String, required: true },
+    players: [{ type: String, required: true }],
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model("Team", teamSchema)
--- models/Ticket.js
+++ models/Ticket.js
@@ -1,2 +1,15 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const ticketSchema = new Schema(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: "User" },
+    matchId: { type: Schema.Types.ObjectId, ref: "Match" },
+    price: { type: Number, required: true },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model("Team", ticketSchema)
