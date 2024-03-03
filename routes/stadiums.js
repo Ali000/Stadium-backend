6cfedc8860c9e65f49674a3fdd1069f70211c7c4
aboodabdo347 <aboodabdo347@gmail.com>
2024-03-03 14:01:54 +0300

    fix some typeo and login

--- routes/stadiums.js
+++ routes/stadiums.js
@@ -1,16 +1,22 @@
-var express = require("express")
+var express = require('express')
 var router = express.Router()
-const stadiumController = require("../controllers/stadiums")
+const stadiumController = require('../controllers/stadiums')
+const middleware = require('../middleware')
 
 //send all stadiums
-router.get("/", stadiumController.index)
+router.get(
+  '/',
+  middleware.stripToken,
+  middleware.verifyToken,
+  stadiumController.index
+)
 //send a stadium
-router.get("/:id", stadiumController.show)
+router.get('/:id', stadiumController.show)
 // update stadium
-router.put("/:id", stadiumController.updateStadium)
+router.put('/:id', stadiumController.updateStadium)
 // create a stadium
-router.post("/", stadiumController.newStadium)
+router.post('/', stadiumController.newStadium)
 // delete a stadium
-router.delete("/:id", stadiumController.deleteStadium)
+router.delete('/:id', stadiumController.deleteStadium)
 
 module.exports = router
