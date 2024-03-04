5d2c75f14addea238c257c25a6b93a62cc60fac2
aboodabdo347 <aboodabdo347@gmail.com>
2024-03-04 06:05:25 +0300

    add the bookings prop to Stadium model

--- models/Stadium.js
+++ models/Stadium.js
@@ -1,17 +1,23 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const stadiumSchema = new Schema(
   {
     name: { type: String, required: true },
     seats: { type: Number, required: true },
-    location: { type: String, required: true},
+    location: { type: String, required: true },
     sport: { type: String, required: true },
-    matches: [{ type: Schema.Types.ObjectId, ref: "Stadium" }],
+    matches: [{ type: Schema.Types.ObjectId, ref: 'Stadium' }],
+    bookings: [
+      {
+        from: { type: Date, required: true },
+        to: { type: Date, required: true }
+      }
+    ]
   },
   {
-    timestamps: true,
+    timestamps: true
   }
 )
 
-module.exports = mongoose.model("Stadium", stadiumSchema)
+module.exports = mongoose.model('Stadium', stadiumSchema)
